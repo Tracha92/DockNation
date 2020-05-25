9f86bdf5403e6a124525a6cbe963bbe6a4dbb68e
tracha1992 <kw12712@gmail.com>
2020-05-25 18:06:58 +0200

    cssy

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,36 +1,26 @@
 import React from "react";
 import Menu from "../Menu/Menu";
 import Network from "../Network/Network";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 import './Main.css';
 
 export default class Main extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            menuSelected: "Network"
-        };
-    }
-
     render() {
-        switch (this.state.menuSelected) {
-            case "Network":
-                return renderNetwork()
-        }
-
         return (
             <div className="Main">
-                <Menu />
+                <Router>
+                    <Menu />
+                    <Switch>
+                        <Route path="/" />
+                        <Route path="/containers" />
+                        <Route path="/images" />
+                        <Route path="/networks" component={Network} />
+                        <Route path="/dockerhub" />
+                        <Route path="/settings" />
+                    </Switch>
+                </Router>
             </div>
         )
     }
-}
-
-function renderNetwork() {
-    return (
-        <div className="Main">
-            <Menu />
-            <Network />
-        </div>
-    )
 }
--- src/containers/Menu/Menu.js
+++ src/containers/Menu/Menu.js
@@ -1,17 +1,66 @@
 import React from 'react';
 import './Menu.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee, faNetworkWired } from '@fortawesome/free-solid-svg-icons'
+import { faBoxOpen, faNetworkWired, faCogs, faClone } from '@fortawesome/free-solid-svg-icons'
 import { faDocker } from "@fortawesome/free-brands-svg-icons";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams,
+    NavLink
+} from "react-router-dom";
 
 export default class Menu extends React.Component {
     render() {
         return <div className="Menu">
-            <div className="MenuItem"><FontAwesomeIcon icon={faCoffee}/>Images</div>
-            <div className="MenuItem">Containers</div>
-            <div className="MenuItem ActiveItem"><FontAwesomeIcon icon={faNetworkWired}/>Networks</div>
-            <div className="MenuItem"><FontAwesomeIcon icon={faDocker}/>DockerHub</div>
-            <div className="MenuItem">Settings</div>
+            <ImagesMenuItem />
+            <ContainersMenuItem />
+            <NetworksMenuItem />
+            <DockerHubMenuItem />
+            <SettingsMenuItem />
         </div>
     }
+}
+
+const ImagesMenuItem = () => {
+    return <NavLink exact to="/images">
+        <div className="MenuItem">
+            <FontAwesomeIcon icon={faClone}/><span>Images</span>
+        </div>
+    </NavLink>
+}
+
+const ContainersMenuItem = () => {
+    return <NavLink to="/containers">
+        <div className="MenuItem">
+            <FontAwesomeIcon icon={faBoxOpen}/><span>Containers</span>
+        </div>
+    </NavLink>
+}
+
+const NetworksMenuItem = () => {
+    return <NavLink to="/networks">
+        <div className="MenuItem">
+            <FontAwesomeIcon icon={faNetworkWired}/><span>Networks</span>
+        </div>
+    </NavLink>
+}
+
+const DockerHubMenuItem = () => {
+    return <NavLink to="/dockerhub">
+        <div className="MenuItem">
+            <FontAwesomeIcon icon={faDocker}/><span>DockerHub</span>
+        </div>
+    </NavLink>
+}
+
+const SettingsMenuItem = () => {
+    return <NavLink to="/settings">
+        <div className="MenuItem">
+            <FontAwesomeIcon icon={faCogs}/><span>Settings</span>
+        </div>
+    </NavLink>
 }
